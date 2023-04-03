e983ea69de7aab8b214680618db0e81bdd8e466c
Henry <100628884+henrybethwaite@users.noreply.github.com>
2023-04-04 09:53:15 +1200

    Finished setting up my own files to work inside of

---
+++ editor_v1.js
@@ -0,0 +1,126 @@
+/*
+ * This editor shows the possible faces that can be created
+ */
+
+const canvasWidth = 960;
+const canvasHeight = 500;
+const bg_color = [255];
+let slider1, slider2, slider3, slider4, slider5;
+let slider6, slider7, slider8, slider9, slider10;
+let faceSelector;
+let faceGuideCheckbox;
+
+function setup () {
+
+  // create the drawing canvas, save the canvas element
+  let main_canvas = createCanvas(canvasWidth, canvasHeight);
+  main_canvas.parent('canvasContainer');
+
+  // create sliders
+  slider1 = createSlider(0, 100, 50);
+  slider2 = createSlider(0, 100, 50);
+  slider3 = createSlider(0, 100, 50);
+  slider4 = createSlider(0, 100, 50);
+  slider5 = createSlider(0, 100, 50);
+  slider6 = createSlider(0, 100, 50);
+  slider7 = createSlider(0, 100, 50);
+  slider8 = createSlider(0, 100, 50);
+  slider9 = createSlider(0, 100, 50);
+  slider10 = createSlider(0, 100, 50);
+
+  slider1.parent('slider1Container');
+  slider2.parent('slider2Container');
+  slider3.parent('slider3Container');
+  slider4.parent('slider4Container');
+  slider5.parent('slider5Container');
+  slider6.parent('slider6Container');
+  slider7.parent('slider7Container');
+  slider8.parent('slider8Container');
+  slider9.parent('slider9Container');
+  slider10.parent('slider10Container');
+
+  faceGuideCheckbox = createCheckbox('', false);
+  faceGuideCheckbox.parent('checkbox1Container');
+
+  faceSelector = createSelect();
+  faceSelector.option('1');
+  faceSelector.option('2');
+  faceSelector.option('3');
+  faceSelector.value('1');
+  faceSelector.parent('selector1Container');
+}
+
+
+
+function draw () {
+  strokeWeight(0.2);
+
+  let mode = faceSelector.value();
+
+  background(bg_color);
+
+  let s1 = slider1.value();
+  let s2 = slider2.value();
+  let s3 = slider3.value();
+  let s4 = slider4.value();
+  let s5 = slider5.value();
+  let s6 = slider6.value();
+  let s7 = slider7.value();
+  let s8 = slider8.value();
+  let s9 = slider9.value();
+  let s10 = slider10.value();
+
+  let show_face_guide = faceGuideCheckbox.checked();
+
+  // use same size / y_pos for all faces
+  let face_size = canvasWidth / 5;
+  let face_scale = face_size / 10;
+  let face_y = height / 2;
+  let face_x = width / 2;
+
+  push();
+  translate(face_x, face_y);
+  scale(face_scale);
+
+  push();
+  if (mode == '1') {
+   // draw face using values mapped from 3 sliders
+   let tilt_value = map(s1, 0, 100, -90, 90);
+   let mouth_value = map(s2, 0, 100, 0.5, 10);
+   let eye_value = int(map(s3, 0, 100, 1, 3));
+   orangeAlienFace(tilt_value, eye_value, mouth_value);
+  }
+
+  if (mode == '2') {
+     // let slider value 1 indicate thinness
+     blockyFace(s1);
+  }
+  if (mode == '3') {
+    simplePurpleFace();
+  }
+
+  pop();
+
+  if(show_face_guide) {
+    strokeWeight(0.1);
+    rectMode(CORNER); 
+    noFill()
+    stroke(0, 0, 255);
+    rect(-10, -10, 20, 20);
+    line(  0, -11,  0, -10);
+    line(  0,  10,  0, 11);
+    line(-11,   0,-10,  0);
+    line( 11,   0, 10,  0);
+  }
+
+  pop();
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+  else if (key == '@') {
+    saveBlocksImages(true);
+  }
+}
---
+++ face_code_v1.js
@@ -0,0 +1,82 @@
+/*
+ * This file should contain code that draws your faces.
+ *
+ * Each function takes parameters and draws a face that is within
+ * the bounding box (-10, -10) to (10, 10).
+ *
+ * These functions are used by your final arrangement of faces as well as the face editor.
+ */
+
+
+/*
+ * tilt_value is in degrees
+ * eye_value is an integer number of eyes: either 0, 1, 2, or 3
+ * mouth_value is how open the mouth is and should generally range from 0.5 to 10
+ */
+function orangeAlienFace(tilt_value, eye_value, mouth_value) {
+    const bg_color3 = [255];
+    const fg_color3 = [30];
+  
+    let headSize = 20
+    let eyeSize = 5;
+    let centerX = 0;
+    let Iy = -4
+    let distactBetweenEyes = 5
+    let MouthDrop = 7
+    
+    // rotation in degrees
+    angleMode(DEGREES);
+    rotate(tilt_value);
+  
+   // head
+    noStroke();
+    fill(fg_color3);
+    ellipse(centerX, 0, headSize, headSize);
+  
+    // 2 traditonal eyes
+    if (eye_value === 1 || eye_value == 3) {
+      fill(bg_color3);
+      ellipse(centerX, Iy, eyeSize-1,eyeSize);
+     
+    }
+  // middle eye
+    if (eye_value >= 2) {
+      fill(bg_color3);
+      ellipse(centerX - distactBetweenEyes, Iy, eyeSize);
+      ellipse(centerX + distactBetweenEyes, Iy, eyeSize );
+    }
+  
+    // mouth
+    fill(bg_color3);
+    ellipse(centerX, Iy + MouthDrop, distactBetweenEyes, mouth_value);
+  }
+  
+  
+  function simplePurpleFace() {
+    fill(30);
+    noStroke();
+    // head
+    ellipse(0, 0, 20);
+    // eyes
+    fill(255);
+    ellipse(-3, -3, 3);
+    ellipse( 3, -3, 3);
+  }
+  
+  /*
+   * thinness_value ranges from 0-100 and indicates how thin the face is
+   */
+  function blockyFace(thinness_value) {
+    // head
+    noStroke();
+    fill(30);
+    let head_width = map(thinness_value, 0, 100, 8, 20);
+    rect(-head_width/2, -9, head_width, 18);
+   
+  
+    // eyes
+    fill(255);
+    ellipse(-2, -4, 1);
+    ellipse( 2, -4, 1);
+  }
+  
